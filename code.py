c08501ef31963497be3f40f2f6ec39d4369771b8
Dominik Tarczyński <158773622+DominikTarczynski@users.noreply.github.com>
2025-01-22 14:03:58 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
 # List of strings
-str_list = ["Python", "is", "fun"]
+str_list = ["Python", "is", "fun", "allways"]
 str_from_list = str(str_list)
-print(str_from_list)  # Output: "['Python', 'is', 'fun']"
+print(str_from_list)
